4a4ee8331d13a775882f89fcbeb06f03a41a9074
psatij01 <psatij01@students.cs.ubc.ca>
2024-10-11 00:01:16 -0700

    Added specifications for PeriodLog class

--- src/main/model/PeriodLog.java
+++ src/main/model/PeriodLog.java
@@ -1,5 +1,25 @@
 package model;
 
+import java.util.List;
+
+/*
+ * This class is used to log all period entries and store them.
+ */
 public class PeriodLog {
-    
+    //fields
+
+    /*
+     * EFFECTS: creates an instance of PeriodLog by creating an array list to store PeriodEntry entries.
+     */
+    public PeriodLog() {
+        //stub
+    }
+
+    /*
+     * EFFECTS: returns the list of all period entries that exist in the period log.
+     */
+    public List<PeriodEntry> printAll() {
+        return null; //stub
+    }
+
 }
