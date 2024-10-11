ef5ce9b922a5a029b94def6b1a757a67a138a488
psatij01 <psatij01@students.cs.ubc.ca>
2024-10-10 17:27:14 -0700

    Added tests for PeriodEntry class

--- src/test/model/PeriodEntryTest.java
+++ src/test/model/PeriodEntryTest.java
@@ -1,5 +1,103 @@
 package model;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class PeriodEntryTest {
 
+    private PeriodEntry testEntry1;
+    private LocalDate date1;
+
+    @BeforeEach
+    void runBefore() {
+        date1 = LocalDate.now();
+        testEntry1 = new PeriodEntry(date1);
+    }
+    
+    @Test
+    void testConstructor() {
+        LocalDate date2 = LocalDate.of(2024, 10, 01);
+        PeriodEntry testEntry2 = new PeriodEntry(date2);
+
+        assertEquals(date1, testEntry1.getDate());
+        assertEquals(date2, testEntry2.getDate());
+    }
+
+    @Test
+    void testLogHeaviness() {
+        testEntry1.logHeaviness(1);
+        assertEquals(1, testEntry1.getHeaviness());
+
+        testEntry1.logHeaviness(2);
+        assertEquals(2, testEntry1.getHeaviness());
+
+
+    }
+
+    @Test
+    void testLogPain() {
+        testEntry1.logPain("Back");
+        testEntry1.logPain("Breasts");
+        List<String> result = testEntry1.getPain();
+        assertEquals(2, result.size());
+        assertEquals("Back", result.get(0));
+        assertEquals("Breasts", result.get(1));
+
+        assertTrue(testEntry1.resetPain());
+        result = testEntry1.getPain();
+        assertEquals(0, result.size());
+
+        assertFalse(testEntry1.resetPain());
+    }
+
+    @Test
+    void testLogCollectionMethod() {
+        testEntry1.logCollectionMethod("Tampon", 3);
+        testEntry1.logCollectionMethod("Tampon", 1);
+        assertEquals("Tampon", testEntry1.getCollectionMethod());
+        assertEquals(4, testEntry1.getCollectionNumUsed());
+
+        testEntry1.logCollectionMethod("Pads", 3);
+        assertEquals("Pads", testEntry1.getCollectionMethod());
+        assertEquals(3, testEntry1.getCollectionNumUsed());
+    }
+
+    @Test
+    void testLogFeelings() {
+        testEntry1.logFeelings("Sensitive");
+        testEntry1.logFeelings("Anxious");
+        List<String> result = testEntry1.getFeelings();
+        assertEquals(2, result.size());
+        assertEquals("Sensitive", result.get(0));
+        assertEquals("Anxious", result.get(1));
+
+        assertTrue(testEntry1.resetFeelings());
+        result = testEntry1.getFeelings();
+        assertEquals(0, result.size());
+
+        assertFalse(testEntry1.resetFeelings());
+    }
+
+    @Test
+    void testLogBreastHealth() {
+        testEntry1.logBreastHealth("Swollen");
+        testEntry1.logBreastHealth("Lumpy");
+        List<String> result = testEntry1.getBreastHealth();
+        assertEquals(2, result.size());
+        assertEquals("Swollen", result.get(0));
+        assertEquals("Lumpy", result.get(1));
+
+        assertTrue(testEntry1.resetBreastHealth());
+        result = testEntry1.getBreastHealth();
+        assertEquals(0, result.size());
+
+        assertFalse(testEntry1.resetBreastHealth());
+    }
 }
