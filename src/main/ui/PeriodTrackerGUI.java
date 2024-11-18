0e5553731dde9ad36596723db4dafd2ea18a2587
psatij01 <psatij01@students.cs.ubc.ca>
2024-11-18 03:10:19 -0800

    Added main image to PeriodTrackerGUI.

--- src/main/ui/PeriodTrackerGUI.java
+++ src/main/ui/PeriodTrackerGUI.java
@@ -2,6 +2,11 @@ package ui;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 /*
  * This is the graphical user interface of the PeriodTracker application.
@@ -19,11 +24,12 @@ public class PeriodTrackerGUI {
     private JButton load;
     private JButton clear;
 
-    private final int WIDTH = 900;
-    private final int HEIGHT = 600;
+    private final int WIDTH = 1150;
+    private final int HEIGHT = 800;
 
     /*
-     * EFFECTS: constructs an instrance of the gui and sets up and runs the application.
+     * EFFECTS: constructs an instrance of the gui and sets up and runs the
+     * application.
      */
     public PeriodTrackerGUI() {
         mainWindow = new JFrame();
@@ -31,12 +37,15 @@ public class PeriodTrackerGUI {
     }
 
     /*
-     * EFFECTS: sets up all the components of the application and adds them to the main window.
+     * EFFECTS: sets up all the components of the application and adds them to the
+     * main window.
      */
     private void setup() {
         mainWindowSetup();
         mainMenuSetup();
         lowerMenuSetup();
+        photoSetup();
+
         mainWindow.add(leftMenuPanel, BorderLayout.WEST);
         mainWindow.add(lowerMenuPanel, BorderLayout.SOUTH);
         run();
@@ -55,18 +64,23 @@ public class PeriodTrackerGUI {
         mainWindow.setSize(WIDTH, HEIGHT);
         mainWindow.setLocationRelativeTo(null);
         mainWindow.setVisible(true);
+        mainWindow.setResizable(false);
     }
 
     /*
-     * EFFECTS: sets up the main menu (left) of the application along with the buttons in it.
+     * EFFECTS: sets up the main menu (left) of the application along with the
+     * buttons in it.
      */
     private void mainMenuSetup() {
         leftMenuPanel = new JPanel(new GridLayout(4, 1, 0, 0));
         leftMenuPanel.setPreferredSize(new Dimension(250, HEIGHT));
+        leftMenuPanel.setBackground(new Color(248, 211, 224));
+
         track = new JButton("Track");
         modify = new JButton("Modify");
         analyze = new JButton("Analyze");
         view = new JButton("View");
+
         leftMenuPanel.add(track);
         leftMenuPanel.add(modify);
         leftMenuPanel.add(analyze);
@@ -74,16 +88,45 @@ public class PeriodTrackerGUI {
     }
 
     /*
-     * EFFECTS: sets up the save/load/clear functionality (bottom menu) of the application.
+     * EFFECTS: sets up the save/load/clear functionality (bottom menu) of the
+     * application.
      */
     private void lowerMenuSetup() {
-        lowerMenuPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT, 5, 10));
+        lowerMenuPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT, 0, 0));
+        lowerMenuPanel.setPreferredSize(new Dimension(WIDTH, 50));
+        lowerMenuPanel.setBackground(new Color(248, 211, 224));
+
         save = new JButton("Save");
+        save.setSize(100, 100);
+
         load = new JButton("Load");
+        load.setSize(100, 100);
+
         clear = new JButton("Clear All Data");
+        clear.setSize(100, 100);
         clear.setForeground(Color.RED);
+
         lowerMenuPanel.add(save);
         lowerMenuPanel.add(load);
         lowerMenuPanel.add(clear);
+    }
+
+    /*
+     * Referenced from:
+     * https://stackoverflow.com/questions/299495/how-to-add-an-image-to-a-jpanel
+     * Image taken from:
+     * https://www.eka.care/services/be-in-sync-with-your-cycle-tracking-your-
+     * menstrual-period
+     * EFFECTS: sets up the main image at the center of the application when the app
+     * is first run.
+     */
+    private void photoSetup() {
+        try {
+            BufferedImage myPicture = ImageIO.read(new File("data/images/main_edited.png"));
+            JLabel picLabel = new JLabel(new ImageIcon(myPicture));
+            mainWindow.add(picLabel, BorderLayout.CENTER);
+        } catch (IOException e) {
+            System.out.println("Image file not found.");
+        }
     }
 }
